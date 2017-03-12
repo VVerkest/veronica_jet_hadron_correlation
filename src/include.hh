78dd83cd5bbca6f70fadf247b85319fac222a2ab
VVerkest <vverkest@gmail.com>
2017-03-11 20:29:48 -0500

    move stuff

---
+++ src/include.hh
@@ -0,0 +1,69 @@
+// ROOT Headers
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TF1.h"
+#include "TF2.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TObjArray.h"
+#include "TString.h"
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TClonesArray.h"
+#include "TChain.h"
+#include "TBranch.h"
+#include "TMath.h"
+#include "TRandom.h"
+#include "TRandom3.h"
+#include "TCanvas.h"
+#include "TStopwatch.h"
+
+// STL Headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <limits.h>
+#include <unistd.h>
+
+// Data is read in by TStarJetPico
+// Library, we convert to FastJet::PseudoJet
+// TStarJetPico headers
+#include "TStarJetPicoReader.h"
+#include "TStarJetPicoEvent.h"
+#include "TStarJetPicoEventHeader.h"
+#include "TStarJetPicoEventCuts.h"
+#include "TStarJetPicoPrimaryTrack.h"
+#include "TStarJetPicoTower.h"
+#include "TStarJetPicoTrackCuts.h"
+#include "TStarJetPicoTowerCuts.h"
+#include "TStarJetVectorContainer.h"
+#include "TStarJetVector.h"
+#include "TStarJetPicoTriggerInfo.h"
+#include "TStarJetPicoUtils.h"
+
+// The analysis is run on FastJet::PseudoJets
+// We make use of the jetfinding tools
+// And the convenient FastJet::Selectors
+#include "fastjet/PseudoJet.hh"
+#include "fastjet/ClusterSequence.hh"
+#include "fastjet/ClusterSequenceArea.hh"
+#include "fastjet/ClusterSequencePassiveArea.hh"
+#include "fastjet/ClusterSequenceActiveArea.hh"
+#include "fastjet/ClusterSequenceActiveAreaExplicitGhosts.hh"
+#include "fastjet/Selector.hh"
+#include "fastjet/tools/JetMedianBackgroundEstimator.hh"
+#include "fastjet/tools/Subtractor.hh"
+#include "fastjet/tools/Filter.hh"
+#include "fastjet/FunctionOfPseudoJet.hh"
+
+// Used for year 7 tracking efficiency corrections,
+// if they are being used
+#include "ktTrackEff.hh"
