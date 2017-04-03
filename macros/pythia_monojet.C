fccea28f0c4dbf7fcc559c0f712d9f6d576cc4e1
VVerkest <vverkest@gmail.com>
2017-04-03 17:55:05 -0400

    undo?

--- macros/pythia_monojet.C
+++ macros/pythia_monojet.C
@@ -11,7 +11,7 @@ void pythia_monojet(){
   const Int_t nPtBins = 11;
   double ptBinLo[nPtBins] = { 3, 4, 5, 7,  9,  11, 15, 25, 35, 45, 55 };
   double ptBinHi[nPtBins] = { 4, 5, 7, 9, 11, 15, 25, 35, 45, 55, 65 };
-  TString ptBinString[nPtBins] = { " 3.0-4.0 ", " 4.0-5.0 ", " 5.0-7.0 "," 7.0-9.0 ", " 9.0-11.0", "11.0-15.0", "15.0-25.0", "25.0-35.0", "35.0-45.0", "45.0-55.0", "55.0-65.0" };
+  TString ptBinString[nPtBins] = { "3.0-4.0", "4.0-5.0", "5.0-7.0","7.0-9.0", "9.0-11.0", "11.0-15.0", "15.0-25.0", "25.0-35.0", "35.0-45.0", "45.0-55.0", "55.0-65.0" };
   Int_t i;
   double l, h, events;
   TString importName, ptNameSet, eventsNameSet;
@@ -42,8 +42,8 @@ void pythia_monojet(){
     ppjetEvents[i]->SetName(eventsNameSet);
 
     events = double(leadJetPt[i]->GetEntries());
-    std::cout << "Pythia pt range :  " << ptBinString[i] << "        Number of Events:  " << events << std::endl;
     
+<<<<<<< HEAD
     if ( events != 0 ) {
       leadJetPt[i]->Scale( 1/events );    // DO NOT divide by zero!
       gStyle->SetOptStat(1);
@@ -51,6 +51,17 @@ void pythia_monojet(){
       top->cd();
       leadJetPt[i]->Write();
     }
+=======
+    if ( events != 0 ) { leadJetPt[i]->Scale( 1/events ); }
+    // DO NOT divide by zero!
+       
+    gStyle->SetOptStat(0);
+     
+    // WRITE
+    top->cd();
+    leadJetPt[i]->Write();
+    ppjetEvents[i]->Write();
+>>>>>>> parent of 03fd448... output values
   }
   delete top;
 }
