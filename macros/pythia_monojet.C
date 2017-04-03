19702c4fc486e44d2a0cec385abefd3ae1d78a9f
VVerkest <vverkest@gmail.com>
2017-04-03 14:40:31 -0400

    tab

--- macros/pythia_monojet.C
+++ macros/pythia_monojet.C
@@ -9,39 +9,39 @@ void pythia_monojet(){
   // create a new Root file
   TFile *top = new TFile(fileSaveName,"recreate");
   
-// create a new subdirectory for each plane
- const Int_t nPtBins = 11;
- double ptBinLo[nPtBins] = { 3, 4, 5, 7,  9,  11, 15, 25, 35, 45, 55 };
- double ptBinHi[nPtBins] = { 4, 5, 7, 9, 11, 15, 25, 35, 45, 55, 65 };
- TString ptBinString[nPtBins] = { "3.0-4.0", "4.0-5.0", "5.0-7.0","7.0-9.0", "9.0-11.0", "11.0-15.0", "15.0-25.0", "25.0-35.0", "35.0-45.0", "45.0-55.0", "55.0-65.0" };
- Int_t i;
- TH1D *leadJetPt[nPtBins];
+  // create a new subdirectory for each plane
+  const Int_t nPtBins = 11;
+  double ptBinLo[nPtBins] = { 3, 4, 5, 7,  9,  11, 15, 25, 35, 45, 55 };
+  double ptBinHi[nPtBins] = { 4, 5, 7, 9, 11, 15, 25, 35, 45, 55, 65 };
+  TString ptBinString[nPtBins] = { "3.0-4.0", "4.0-5.0", "5.0-7.0","7.0-9.0", "9.0-11.0", "11.0-15.0", "15.0-25.0", "25.0-35.0", "35.0-45.0", "45.0-55.0", "55.0-65.0" };
+  Int_t i;
+  TH1D *leadJetPt[nPtBins];
     
-   for (i=0;i<nPtBins;i++) {
-     TString importName = "pythia_ppjet_lead_20_max_100__";
-     importName += ptBinLo[i];
-     importName += "_";
-     importName += ptBinHi[i];
-     importName += ".root";
+  for (i=0;i<nPtBins;i++) {
+    TString importName = "pythia_ppjet_lead_20_max_100__";
+    importName += ptBinLo[i];
+    importName += "_";
+    importName += ptBinHi[i];
+    importName += ".root";
 
-     //  IMPORT
-     TFile* ppjetFILE = new TFile( importName, "READ" );
-     TH1D* leadJetPt[i] = (TH1D*) ppjetFILE->Get("ppleadjetpt");
-     TH1D* ppjetEvents[i] = (TH1D*) ppjetFILE->Get("binvzdist");
-     TString nameSet = "pp_jetPt_"
-     nameSet += ptBinLo[i];
-     nameSet += "_";
-     nameSet += ptBinHi[i];
+    //  IMPORT
+    TFile* ppjetFILE = new TFile( importName, "READ" );
+    TH1D* leadJetPt[i] = (TH1D*) ppjetFILE->Get("ppleadjetpt");
+    TH1D* ppjetEvents[i] = (TH1D*) ppjetFILE->Get("binvzdist");
+    TString nameSet = "pp_jetPt_"
+      nameSet += ptBinLo[i];
+    nameSet += "_";
+    nameSet += ptBinHi[i];
 
-     leadJetPt[i]->SetName(nameSet);
-     leadJetPt[i]->Scale( 1/double(ppjetEvents->Integral()) );
+    leadJetPt[i]->SetName(nameSet);
+    leadJetPt[i]->Scale( 1/double(ppjetEvents->Integral()) );
        
-     gStyle->SetOptStat(0);
+    gStyle->SetOptStat(0);
      
-     // WRITE
-     top->cd();
-     leadJetPt[i]->Write();
-   }
+    // WRITE
+    top->cd();
+    leadJetPt[i]->Write();
+  }
 
- delete top;
+  delete top;
 }
