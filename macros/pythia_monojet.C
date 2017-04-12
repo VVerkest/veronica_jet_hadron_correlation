b57f4cb3ff102c2584cb9536cc46ecd2bca961d9
VVerkest <vverkest@gmail.com>
2017-04-12 14:01:12 -0400

    .

--- macros/pythia_monojet.C
+++ macros/pythia_monojet.C
@@ -50,8 +50,8 @@ void pythia_monojet(){
 
     std::cout << " pt bin range:   " << ptBinString[i] << "              number of events:  " << events << std::endl;
     
-    if ( events != 0 ) {
-      leadJetPt[i]->Scale( 1/(6*pi) );    // DO NOT divide by zero!
+    if ( events != 0 ) {    // DO NOT divide by zero!
+      leadJetPt[i]->Scale( 1/(6*pi) );  // divide by 2pi and divide by 3 (delta Eta)
       leadJetPt[i]->Scale( 1/(leadJetPt[i]->GetXaxis()->GetBinWidth(1)) );
       gStyle->SetOptStat(1);
 
